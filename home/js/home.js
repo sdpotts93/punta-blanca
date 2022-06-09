4b50aa004d7f9aad5c226202f5ae183b54d0933f
Steven Potts <sdpotts93@gmail.com>
2022-06-09 17:37:33 -0500

    link to external

--- home/js/home.js
+++ home/js/home.js
@@ -34,7 +34,12 @@ checkoutDatepicker.options.setMinDate(formattedInitialMinCheckoutDate);
 
 let checkoutParams = "";
 
+
+const checkAvailabilityButton = document.querySelector("#email-form > input");
+
 // ----------------- LISTENERS ----------------------
+
+checkAvailabilityButton.addEventListener("click", handleCheckAvailability, false);
 
 checkinDate.addEventListener("input", maskDateInput, false);
 
@@ -43,6 +48,8 @@ checkoutDate.addEventListener("input", maskDateInput, false);
 checkinDatepicker.options.onSelect((event, day, previousDay) => {
 
   if (!day) {
+
+    checkinParams = "";
 
     // Create new Date instance
     const initialMinCheckoutDate = new Date(Date.UTC(
@@ -76,6 +83,8 @@ checkoutDatepicker.options.onSelect((event, day, previousDay) => {
 
   if (!day) {
 
+    checkoutParams = "";
+
     checkinDatepicker.options.setMaxDate(null);
 
     checkoutDate.value = "";
@@ -98,6 +107,14 @@ checkoutDatepicker.options.onSelect((event, day, previousDay) => {
 
 // ----------------- FUNCTIONS ----------------------
 
+function handleCheckAvailability(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (checkoutParams && checkinParams) {
+    window.location.href = `https://hotels.cloudbeds.com/reservation/HkDshd#checkin=${checkinParams}&checkout=${checkoutParams}`
+  }
+}
+
 function formatISODate(date) {
   const dateArray = date.split("-");
   return `${dateArray[1]}/${dateArray[2]}/${dateArray[0]}`
@@ -111,71 +128,5 @@ function padNumber(n) {
 
 // Mask date input to match format DD/MM/YYYY
 function maskDateInput(e) {
-
-  const selectionStart = this.selectionStart;
-
-  // Only grab digits
-  const onlyDigits = this.value.replace(/\D/g,'');
-
-  // If the user writes in the middle of the input, this will push
-  // the last digit out (FIFO)
-  const maxEightDigits = onlyDigits.substring(0, Math.min(8, onlyDigits.length));
-
-  // Get an array that looks like this ['DD', 'MM', 'YYYY']
-  const date = splitString(maxEightDigits, 2, 4);
-
-  // Join that array with "/" to get the correct date input format
-  const joinedDate = date.join("/");
-
-  this.value = joinedDate;
-
-  // Separate date in parts
-  const dateParts = joinedDate.split("/");
-
-  // Create date object from user input (DD/MM/YYYY)
-  // Month is 0-based, that's why we need dataParts[1] - 1
-  const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
-
-  // Selected date with format YYYY/MM/DD
-  const selectedDate = dateObject.getFullYear()+'/'+ (dateObject.getMonth()+1) +'/'+ dateObject.getDate();
-
-  // Get current datetime with timezone
-  const nowDate = new Date();
-
-  // Current datetime with format YYYY/MM/DD
-  const currentDate = nowDate.getFullYear()+ '/' + (nowDate.getMonth()+1) + '/' + nowDate.getDate();
-
-  // Get the number of days in a particular month
-  const daysInMonth = new Date(+dateParts[2], dateParts[1] - 1, 0).getDate();
-
-  try {
-    if (
-      (new Date(selectedDate) > new Date(currentDate)) ||
-      +dateParts[0] > daysInMonth ||
-      dateParts[1] - 1 > 11
-    ) {
-      dateError.style.display = "block";
-      wrongDate = true;
-    } else {
-
-      const dateObject = new Date(selectedDate).getTime();
-
-      const tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-
-      const localISOTime = new Date(dateObject - tzoffset).toISOString()
-
-      dateHiddenInput.value = localISOTime;
-
-      dateError.style.display = "none";
-      wrongDate = false;
-    }
-  } catch {
-    // If there are still not three parts to the input date, hide date warning
-    dateError.style.display = "none";
-  }
-
-  // If the date value was already filled, return cursor to the same position
-  if (/^\d{2}\/\d{2}\/\d{4}$/.test(this.value)) {
-    this.selectionStart = this.selectionEnd = selectionStart;
-  }
+  return false;
 }
